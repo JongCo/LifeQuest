a0c6e8c071745e2df1ca01e1e7c32d48d6856412
JongCo <xn1128@gmail.com>
2022-04-06 23:48:14 +0900

    listen 함수의 port Argument가 변수를 참조하도록 변경

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const db = require("./dbinit");
 
 const express = require('express'); //express 모듈 import
 const app = express(); //express app선언
-const port = 3000 //port는 일단 3000으로 하자
+const port = 30000 //port는 일단 3000으로 하자
 
 
 app.set('views', path.join(__dirname,'/views')); //views 의 경로를 지정
@@ -34,6 +34,6 @@ const router = require('./router/main')(app)
 //미들웨어가 post request를 파싱한 후 이 라우터에 전달한다
 //라고 이해하면 되나
 
-const server = app.listen(30000, function() {
+const server = app.listen(port, function() {
     console.log(`Express server has started on port ${port}`);
 })
