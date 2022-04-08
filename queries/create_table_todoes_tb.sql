8513ff1ac44cbebe80397c38cdf8fc7c37dde8a8
JongCo <xn1128@gmail.com>
2022-04-08 10:51:16 +0900

    todoes 테이블의 Unique제약이 title, uid 다중으로 걸리도록 구조 수정

--- queries/create_table_todoes_tb.sql
+++ queries/create_table_todoes_tb.sql
@@ -1,9 +1,10 @@
 CREATE TABLE "todoes_tb" (
 	"title_id"	INTEGER,
-	"title"	TEXT NOT NULL UNIQUE,
+	"title"	TEXT NOT NULL,
 	"create_date"	TEXT,
 	"uid"	INTEGER NOT NULL,
 	"success"	TEXT NOT NULL DEFAULT "false",
 	PRIMARY KEY("title_id" AUTOINCREMENT),
-	FOREIGN KEY("uid") REFERENCES "users_tb"("uid")
-)
+	FOREIGN KEY("uid") REFERENCES "users_tb"("uid"),
+	UNIQUE("title", "uid")
+);
