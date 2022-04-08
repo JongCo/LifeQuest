78deecc37b71e152d2c011f21ea1f1c8fb8f051a
JongCo <xn1128@gmail.com>
2022-04-08 10:02:35 +0900

    회원가입 후 로그인창으로 넘어가게 하기 / 로그인페이지 회원가입링크 위치 조정

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,14 +1,20 @@
 const xhr = new XMLHttpRequest();
 
+xhr.addEventListener("load", e => {
+    if(xhr.status == 200){
+        alert("회원가입을 축하드립니다 !!");
+        window.location.href = './login';
+    } else {
+        alert("회원가입 실패하였습니다..");    
+    }
+});
+
 window.addEventListener("load", e => {
 
     const usernameInput = document.getElementById("username-input");
     const passwordInput = document.getElementById("password-input");
     const signupButton = document.getElementById("signup-btn");
 
-    xhr.addEventListener("load", e => {
-        console.log("signup.js:17 " + xhr.responseText);
-    });
     
     signupButton.addEventListener("click", e => {
         signupInfoJson = {
